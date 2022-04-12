2c730e096776a4b2e9b430797fe58724358fd2a7
prabath1998 <prabhathudayanga1998@gmail.com>
2022-04-12 23:26:34 +0530

    Added useStates

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,28 @@
-const Home = () => {
+import { useState } from "react";
+
+const Home = (e) => {
+  // let name = "janaka";
+  const [name, setName] = useState("janaka");
+  const [age, setAge] = useState(25);
+
+  const handleClick = () => {
+    setName("chathuranga");
+    setAge(52);
+  };
+
+  const handleClickAgain = (name, e) => {
+    console.log("hello " + name, e.target);
+  };
   return (
     <div className="home">
       <h2>Home Page</h2>
+      <button onClick={handleClick}>Click Me</button>
+      <p>
+        {name} is {age} years old
+      </p>
+      <button onClick={(e) => handleClickAgain("prabath", e)}>
+        Click Again
+      </button>
     </div>
   );
 };
